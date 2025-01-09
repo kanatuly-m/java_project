851ca56e135c07aee9158bdca69d9d549b629ecc
kanatuly-m <240941@astanait.edu.kz>
2025-01-09 13:11:13 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Film film1 = new Film("Interstellar", "Epic scienceiction drama", 169);
+        Film film1 = new Film("Interstellar", "Sci-Fi", 169);
         Film film2 = new Film("Titanic", "Romance", 195);
-        Viewer viewer1=new Viewer("Islana",17);
-        Viewer viewer2=new Viewer("Merey", 17);
-        Cinema cinema1=new Cinema("Chaplin", 200);
-        Cinema cinema2=new Cinema("Kinopark", 150);
-        System.out.println("** Films **");
-        film1.displayFilmInfo();
-        film2.displayFilmInfo();
-        System.out.println("\n** Viewers **");
-        viewer1.displayViewerInfo();
-        viewer2.displayViewerInfo();
-        System.out.println("\n** Cinemas **");
-        cinema1.displayCinemaInfo();
-        cinema2.displayCinemaInfo();
-        System.out.println("\nComparing Films:");
-        if (film1.getDuration() > film2.getDuration()) {
-            System.out.println(film1.getTitle()+" is longer than "+film2.getTitle());
-        } else if (film1.getDuration() < film2.getDuration()) {
-            System.out.println(film2.getTitle()+" is longer than "+film1.getTitle());
-        } else {
-            System.out.println("Both films have the same duration.");
-        }
+        Viewer viewer1 = new Viewer("Alice", 25);
+        Viewer viewer2 = new Viewer("Bob", 30);
+        Cinema cinema1 = new Cinema("Grand Cinema", 200);
+        Cinema cinema2 = new Cinema("Cityplex", 150);
+
+        System.out.println(film1);
+        System.out.println(film2);
+        System.out.println(viewer1);
+        System.out.println(viewer2);
+        System.out.println(cinema1);
+        System.out.println(cinema2);
+
+        System.out.println("Are films equal? " + film1.equals(film2));
+
+        List<Film> films = Arrays.asList(film1, film2);
+        films.sort((f1, f2) -> Integer.compare(f1.getDuration(), f2.getDuration()));
+        System.out.println("Sorted films by duration: " + films);
     }
 }

94a35a5edcac05f6fc41e11af8bd7101718263fc
kanatuly-m <240941@astanait.edu.kz>
2025-01-23 13:37:02 +0500

    Update Booking.java

--- src/Booking.java
+++ src/Booking.java
@@ -10,23 +10,9 @@ public class Booking {
         this.status = status;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public int getFilmId() {
-        return filmId;
-    }
-
-    public int getViewerId() {
-        return viewerId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    public int getId() { return id; }
+    public int getFilmId() { return filmId; }
+    public int getViewerId() { return viewerId; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }

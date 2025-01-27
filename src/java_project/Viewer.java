4be3d0656532924b4135b82d32ddddd247ed7b9e
kanatuly-m <240941@astanait.edu.kz>
2025-01-27 23:40:10 +0500

    Create Viewer.java

---
+++ src/java_project/Viewer.java
@@ -0,0 +1,36 @@
+package java_project;
+public class Viewer extends Entity {
+    private String name;
+    private int age;
+
+    public Viewer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    @Override
+    public String toString() {
+        return "Viewer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Viewer viewer = (Viewer) obj;
+        return age == viewer.age && name.equals(viewer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + age;
+    }
+}

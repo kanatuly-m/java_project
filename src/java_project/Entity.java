0f8280b2b37aa72fc73474bda4f60a5468c0f98e
kanatuly-m <240941@astanait.edu.kz>
2025-01-27 23:40:26 +0500

    Create Entity.java

---
+++ src/java_project/Entity.java
@@ -0,0 +1,6 @@
+package java_project;
+public abstract class Entity {
+    public abstract String toString();
+    public abstract boolean equals(Object obj);
+    public abstract int hashCode();
+}
